e59f93c80127cbf8088ebe3beb7007553257a184
Jannis R <mail@jannisr.de>
2017-09-10 15:39:59 +0200

    implementation :clap:, tests :white_check_mark:

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 'use strict'
 
-// todo
+const LiveMovingAverage = {
+	push: function (val) {
+		if ('number' !== typeof val) throw new Error('val must be a number.')
+
+		this.sum -= this.data[this.dataI]
+		this.sum += val
+
+		this.data[this.dataI] = val
+		this.dataI = (this.dataI + 1) % this.size
+	},
+
+	get: function () {
+		return this.sum / this.size
+	}
+}
+
+const createWindow = (size, fill = 0) => {
+	const w = Object.create(LiveMovingAverage)
+
+	w.sum = size * fill
+	w.size = size
+	w.data = new Array(size)
+	w.data.fill(fill)
+	w.dataI = 0
+
+	return w
+}
+
+module.exports = createWindow
--- test.js
+++ test.js
@@ -2,7 +2,28 @@
 
 const test = require('tape')
 
-const todo = require('.')
+const createWindow = require('.')
 
-test('todo', (t) => {
+test('works', (t) => {
+	const avg = createWindow(3, 0)
+
+	t.equal(avg.get(), 0)
+
+	avg.push(2)
+	avg.push(3)
+	avg.push(4)
+	t.equal(avg.get(), 3) // (2 + 3 + 4) / 3
+
+	avg.push(5)
+	t.equal(avg.get(), 4) // (3 + 4 + 5) / 3
+
+	avg.push(6)
+	t.equal(avg.get(), 5) // (4 + 5 + 6) / 3
+
+	avg.push(7)
+	t.equal(avg.get(), 6) // (5 + 6 + 7) / 3
+
+	t.end()
 })
+
+// todo: test with fill !== 0
