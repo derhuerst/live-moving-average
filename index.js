e4779aa1e77a306b297063539e5cd108e8e2e634
Jannis R <mail@jannisr.de>
2018-06-06 11:05:59 +0200

    validate arg types :bug:

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ const LiveMovingAverage = {
 }
 
 const createWindow = (size, fill = 0) => {
+    if ('number' !== typeof size) throw new Error('size must be a number.')
+    if ('number' !== typeof fill) throw new Error('fill must be a number.')
+
 	const w = Object.create(LiveMovingAverage)
 
 	w.sum = size * fill
