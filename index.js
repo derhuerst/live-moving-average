3a33c89e4dfa65c5ab410a23343ccc22e9021e55
Wayne Parrott <5588978+wayneparrott@users.noreply.github.com>
2020-09-29 11:04:29 -0500

    corrected code indent styling to 4 spaces

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 'use strict'
 
 const LiveMovingAverage = {
-	push: function (val) {
-		if ('number' !== typeof val) throw new Error('val must be a number.')
+    push: function (val) {
+        if ('number' !== typeof val) throw new Error('val must be a number.')
 
-		this.sum -= this.data[this.dataI]
-		this.sum += val
+        this.sum -= this.data[this.dataI]
+        this.sum += val
 
-		this.data[this.dataI] = val
-    this.dataI = (this.dataI + 1) % this.size
-    
-    return this
-	},
+        this.data[this.dataI] = val
+        this.dataI = (this.dataI + 1) % this.size
 
-	get: function () {
-		return this.sum / this.size
-	}
+        return this
+    },
+
+    get: function () {
+        return this.sum / this.size
+    }
 }
 
 const createWindow = (size, fill = 0) => {
     if ('number' !== typeof size) throw new Error('size must be a number.')
     if ('number' !== typeof fill) throw new Error('fill must be a number.')
 
-	const w = Object.create(LiveMovingAverage)
+    const w = Object.create(LiveMovingAverage)
 
-	w.sum = size * fill
-	w.size = size
-	w.data = new Array(size)
-	w.data.fill(fill)
-	w.dataI = 0
+    w.sum = size * fill
+    w.size = size
+    w.data = new Array(size)
+    w.data.fill(fill)
+    w.dataI = 0
 
-	return w
+    return w
 }
 
 module.exports = createWindow
